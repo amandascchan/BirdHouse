05a0cf722de7ca65f7c15519a81b88e142174f9e
amandascchan <amandascchan@gmail.com>
2016-08-06 21:49:45 -0400

    messing up all the aligning

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,6 +37,22 @@
         });
 
 
+        $(function() {
+            $('a[href*="#"]:not([href="#"])').click(function() {
+                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                    if (target.length) {
+                        $('html, body').animate({
+                        scrollTop: target.offset().top
+                        }, 1000);
+                        return false;
+                    }
+                }
+            });
+        });
+
+
 		// Prioritize "important" elements on narrower.
 			skel.on('+narrower -narrower', function() {
 				$.prioritize(
